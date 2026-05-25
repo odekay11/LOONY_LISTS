c97ee705884b2192d5450aa3a607ab70cb493ac9
odekay11 <odekay@scu.edu>
2026-05-24 18:26:46 -0700

    Implemented createList function

--- list.c
+++ list.c
@@ -33,8 +33,53 @@ typedef struct node{
     struct NoDE *prev;//previous node
 
 }NODE;
+
 typedef struct list{
     int count; //total number of elements stored in the list 
     struct LIST *head;//first node 
     struct LIST *tail;//last node
 }LIST;
+
+ LIST *createList(void){
+
+ }
+
+ void destroyList(LIST *lp){
+
+ }
+
+ int numItems(LIST *lp){
+
+ }
+
+ void addFirst(LIST *lp, void *item){
+
+ }
+
+ void addLast(LIST *lp, void *item){
+
+ }
+
+ void *removeFirst(LIST *lp){
+
+ }
+
+ void *removeLast(LIST *lp){
+
+ }
+
+ void *getFirst(LIST *lp){
+
+ }
+
+ void *getLast(LIST *lp){
+
+ }
+
+ void *getItem(LIST *lp, int index){
+
+ }
+
+ void setItem(LIST *lp, int index, void *item){
+    
+ }
