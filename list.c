ed6ae6d5c7e45734c074390ef2299cb67c69e0b1
odekay11 <odekay@scu.edu>
2026-05-24 18:34:29 -0700

    Implemented destroyList

--- list.c
+++ list.c
@@ -2,35 +2,22 @@
 File: list.c
 Author: Owen DeKay
 */
-//Implement a doubly linked list and a circular queue
 
-//Should the linked list be singly-linked or doubly-linked?
-//Doubly so that operations are more efficient, if we addLast is called or removeLast the time complexity is O(1)
-
-//Do we need only a head pointer or both a head and a tail pointers?
-//Both if we want to implement a doubly linked list. THis will improve effiency as we will not need to iterate through the list to get to the end.
-//Without addLast and removeLast become O(N)
-
-//Should the arrays within the nodes all be the same size, or should nodes increase in size as we add more and more elements?
-//Fixed size so that we never have to resize which will cause efficiency losses.
-//Indexing is simple
-//Space is not wasted
-
-/*indexing function to find the last postion:  Rather we keep track of the first slot, f ,
- *and the items are stored starting at slot f and continue through slot ( f + n − 1) mod m,
- *f=index of first item
- *n=number of items
- *m=size of array
- */
-
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "list.h"
+#define INTIAL_NODE_SIZE 2
 
 
 typedef struct node{
     void **data; //pointer to array of void pointers of fixed size
     int count; //number of elements stored at a given node
     int first;//index of the first element in the circular queue
+    int size;//how many slots are in a node
     struct NODE *next; //next node 
-    struct NoDE *prev;//previous node
+    struct NODE *prev;//previous node
 
 }NODE;
 
@@ -41,11 +28,44 @@ typedef struct list{
 }LIST;
 
  LIST *createList(void){
+    //allocate list struct
+    LIST *lp=malloc(sizeof(LIST));
+    assert(lp!=NULL);
+
+    //allocate first node
+    NODE *node=malloc(sizeof(NODE));
+    assert(node!=NULL);
+
+    node->data=malloc(sizeof(void *)*INTIAL_NODE_SIZE);
+    assert(node->data!=NULL);
+
+    node->size=INTIAL_NODE_SIZE;
+    node->first=0;
+    node->count=0;
+    node->next=NULL;
+    node->prev=NULL;
+
+    lp->count=0;
+    lp->head=node;
+    lp->tail=node;
+
 
  }
 
  void destroyList(LIST *lp){
-
+    assert(lp!=NULL);
+    NODE *current=lp->head;
+    NODE *temp;
+    while(current!=NULL){
+        temp=current;
+        current=current->next;
+        //free data array
+        free(temp->data);
+        //free node
+        free(temp);
+    }
+    //free list
+    free(lp);
  }
 
  int numItems(LIST *lp){
@@ -81,5 +101,5 @@ typedef struct list{
  }
 
  void setItem(LIST *lp, int index, void *item){
-    
+
  }
